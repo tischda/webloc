acca98277e7852654a878edc8d23c74052a56ac6
Daniel TISCHER <dos.7182@gmail.com>
2015-03-14 18:40:44 +0100

    go fmt

--- decode.go
+++ decode.go
@@ -1,79 +1,80 @@
 package main
+
 import (
-    "path/filepath"
-    "howett.net/plist"
-    "os"
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
+	"howett.net/plist"
+	"os"
+	"path/filepath"
 )
 
 type weblocHeader struct {
-    URL string `plist:"URL"`
+	URL string `plist:"URL"`
 }
 
 var delete bool
 var noop bool
 
 func init() {
-    flag.BoolVar(&delete, "delete", false, "delete .webloc files after conversion")
-    flag.BoolVar(&noop, "noop", false, "decode urls, but do not change file system")
-    flag.Parse()
+	flag.BoolVar(&delete, "delete", false, "delete .webloc files after conversion")
+	flag.BoolVar(&noop, "noop", false, "decode urls, but do not change file system")
+	flag.Parse()
 }
 
 func main() {
-    root := flag.Arg(0)
-    filepath.Walk(root, walkpath)
+	root := flag.Arg(0)
+	filepath.Walk(root, walkpath)
 }
 
 func walkpath(path string, f os.FileInfo, err error) error {
-    if err != nil {
-        return err
-    }
-    matched, err := filepath.Match("*.webloc", f.Name())
-    if err != nil {
-        return err
-    }
-    if matched {
-        process(path)
-    }
-    return nil
+	if err != nil {
+		return err
+	}
+	matched, err := filepath.Match("*.webloc", f.Name())
+	if err != nil {
+		return err
+	}
+	if matched {
+		process(path)
+	}
+	return nil
 }
 
 func process(path string) {
-    url := decode(path)
-    fmt.Println(url)
+	url := decode(path)
+	fmt.Println(url)
 
-    if ! noop {
-        writeUrl(path[:len(path)-len(".webloc")] + ".url", url)
-        if delete {
-            err := os.Remove(path)
-            check(err)
-        }
-    }
+	if !noop {
+		writeUrl(path[:len(path)-len(".webloc")]+".url", url)
+		if delete {
+			err := os.Remove(path)
+			check(err)
+		}
+	}
 }
 
 func decode(path string) string {
-    var data weblocHeader
+	var data weblocHeader
 
-    f, err := os.Open(path)
-    check(err)
-    defer f.Close()
+	f, err := os.Open(path)
+	check(err)
+	defer f.Close()
 
-    decoder := plist.NewDecoder(f)
-    check(decoder.Decode(&data))
-    return data.URL
+	decoder := plist.NewDecoder(f)
+	check(decoder.Decode(&data))
+	return data.URL
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func writeUrl(path string, url string) {
-    f, err := os.Create(path)
-    check(err)
-    defer f.Close()
+	f, err := os.Create(path)
+	check(err)
+	defer f.Close()
 
-    f.WriteString("[InternetShortcut]\nURL=" + url)
+	f.WriteString("[InternetShortcut]\nURL=" + url)
 }
