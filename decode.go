7e21613e1b870b1ac872776c43faf9c70b331f0f
Daniel TISCHER <dos.7182@gmail.com>
2015-03-14 18:24:24 +0100

    added options

--- decode.go
+++ decode.go
@@ -2,42 +2,57 @@ package main
 import (
     "path/filepath"
     "howett.net/plist"
-    "fmt"
     "os"
     "flag"
+    "fmt"
 )
 
 type weblocHeader struct {
     URL string `plist:"URL"`
 }
 
-func main() {
+var delete bool
+var noop bool
+
+func init() {
+    flag.BoolVar(&delete, "delete", false, "delete .webloc files after conversion")
+    flag.BoolVar(&noop, "noop", false, "decode urls, but do not change file system")
     flag.Parse()
+}
+
+func main() {
     root := flag.Arg(0)
     filepath.Walk(root, walkpath)
 }
 
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
-
 func walkpath(path string, f os.FileInfo, err error) error {
     if err != nil {
-        return nil
+        return err
     }
     matched, err := filepath.Match("*.webloc", f.Name())
     if err != nil {
         return err
     }
     if matched {
-        decode(path)
+        process(path)
     }
     return nil
 }
 
-func decode(path string) {
+func process(path string) {
+    url := decode(path)
+    fmt.Println(url)
+
+    if ! noop {
+        writeUrl(path[:len(path)-len(".webloc")] + ".url", url)
+        if delete {
+            err := os.Remove(path)
+            check(err)
+        }
+    }
+}
+
+func decode(path string) string {
     var data weblocHeader
 
     f, err := os.Open(path)
@@ -46,5 +61,19 @@ func decode(path string) {
 
     decoder := plist.NewDecoder(f)
     check(decoder.Decode(&data))
-    fmt.Println(data)
+    return data.URL
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func writeUrl(path string, url string) {
+    f, err := os.Create(path)
+    check(err)
+    defer f.Close()
+
+    f.WriteString("[InternetShortcut]\nURL=" + url)
 }
