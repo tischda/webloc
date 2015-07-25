fe6a34baf0d152d5525cb9cc80a76ea48bde114b
Daniel TISCHER <dos.7182@gmail.com>
2015-07-25 19:57:18 +0200

    added build scripts

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 )
 
-const version string = "1.0.0"
+// http://technosophos.com/2014/06/11/compile-time-string-in-go.html
+// go build -ldflags "-x main.version $(git describe --tags)"
+var version string
 
 type weblocHeader struct {
 	URL string `plist:"URL"`
