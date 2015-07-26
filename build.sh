a7e289eff5ee01e45b0d8d9527fcc417c023f962
Daniel TISCHER <dos.7182@gmail.com>
2015-07-26 21:38:48 +0200

    fix

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ go clean
 
 # doc says not to use -s to remove the debug info read by gdb
 # but https://github.com/golang/go/issues/6245 says it's fixed
-go build -ldflags "-X main.version $(git describe --tags)" -s
+go build -ldflags "-X main.version $(git describe --tags) -s"
 
 # building with -race makes the binary huge!
